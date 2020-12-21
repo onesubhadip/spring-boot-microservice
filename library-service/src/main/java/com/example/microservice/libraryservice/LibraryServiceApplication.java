deae69ec8ffd5263f6fa7d909c1f73f9cb55b8ec
Subhadip Dey <subhadip.dey@telenetgroup.be>
2020-12-21 17:27:47 +0530

    library-service :: register to Eureka discovery server

--- library-service/src/main/java/com/example/microservice/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/com/example/microservice/libraryservice/LibraryServiceApplication.java
@@ -1,36 +1,19 @@
 package com.example.microservice.libraryservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableEurekaClient
 public class LibraryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryServiceApplication.class, args);
-	}
-
-	@Autowired
-	private Environment environment;
-
-	public void getActiveProfiles() {
-		for (String profileName : environment.getActiveProfiles()) {
-			System.out.println("Currently active profile - " + profileName);
-		}
-	}
-
-	@Bean
-	public String test() {
-		getActiveProfiles();
-		return "test";
 	}
 
 }
